9f6e0f48c76c3371e92c00bd20da603e9325d3a7
Jose Miguel <granadero98@gmail.com>
2021-03-14 17:42:17 +0100

    Update main.py
    Updated

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import sys
 import requests
 import urllib.request
-import zipfile
 import shutil
 
 # -----------------------------------------------
@@ -17,40 +16,27 @@ if rutaArchivo == "RUTA":
 # -----------------------------------------------
 
 # Obtener version actual del launcher
-version = str(requests.get(
-    "http://akamai-gamecdn.blackdesertonline.com/live001/game/config/config.language.version").json())
+versionList = requests.get(
+    "http://dn.sea.playblackdesert.com/UploadData/ads_files").text.split()
+version = versionList[versionList.index('languagedata_en.loc') + 1]
 print("Version: " + version)
 
 # Añadir version a URL
-url = "http://akamai-gamecdn.blackdesertonline.com/live001/game/language/BDOLanguage_{0}.zip".format(
+url = "http://dn.sea.playblackdesert.com/UploadData/ads/languagedata_en/{0}/languagedata_en.loc".format(
     version)
 print("Url: " + url)
 
-# Nombre archivo
-nombreConExtension = "{0}.zip".format(version)
-
-# Comprobar que el archivo no esta ya descargado
-if os.path.isfile(nombreConExtension):
-    sys.exit("ERROR: Ya tienes esta version descargada, borra el zip con el nombre: {0}".format(nombreConExtension))
-
 # Descargar archivo
-urllib.request.urlretrieve(url, nombreConExtension)
-
-# Extraer archivo
-with zipfile.ZipFile(nombreConExtension, "r") as zip_ref:
-    zip_ref.extractall(version)
+urllib.request.urlretrieve(url, 'languagedata_en.loc')
 
 # Cambiar nombre archivo
-os.rename("{0}/languagedata_en.loc".format(version), "{0}/languagedata_es.loc".format(version))
+os.rename("languagedata_en.loc", "languagedata_es.loc")
 
 # Comprobar si el archivo existe y borrar si existe en el target
 if os.path.isfile('{0}/languagedata_es.loc'.format(rutaArchivo)):
     os.remove('{0}/languagedata_es.loc'.format(rutaArchivo))
 
 # Mover archivo
-shutil.move("{0}/languagedata_es.loc".format(version), rutaArchivo)
-
-# Borrar carpeta descomprimida
-shutil.rmtree(version)
+shutil.move("languagedata_es.loc", rutaArchivo)
 
 print("Good job, all worked as expected ofc")
